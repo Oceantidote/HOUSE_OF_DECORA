8349685691591ce09363b4283005db756269ee92
Ife Odugbesan <ifeodugbesan@yahoo.com>
2019-09-09 17:15:55 +0100

    applied gonchy's changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :about, :services, :contact, :testimonials, :gallery]
+  skip_before_action :authenticate_user!, only: [:home, :about, :services, :contact, :testimonials, :gallery, :send_email]
 
   def home
   end
@@ -20,5 +20,12 @@ class PagesController < ApplicationController
   end
 
   def send_email
+    contacts = params["contact"]
+    if ContactMailer.send_email_to_contact(contacts).deliver_now
+      flash[:alert] = 'Your message has been sent'
+    else
+      render '/contacts'
+    end
+    redirect_to root_path
   end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  # default from: 'contact@houseofdecora.com'
+  default from: 'ifeodugbesan@yahoo.com'
   layout 'mailer'
 end
